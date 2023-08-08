76a246a1c2cd1be1b0da0826582489e64105d03a
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-08-08 18:31:51 -0400

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const router = require('./routes');
 
 const app = express();
 
-mongoose.connect('mongodb://27017/bitfilmsdb');
+mongoose.connect('mongodb://127.0.0.1:27017/bitfilmsdb');
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(requestLogger);
