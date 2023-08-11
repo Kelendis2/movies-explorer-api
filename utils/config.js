84247eb0db15094e37c8f54c53de5dffee2905b7
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-08-10 21:07:43 -0400

    add limiter and create config

---
+++ utils/config.js
@@ -0,0 +1,25 @@
+const rateLimit = require('express-rate-limit');
+const {
+  PORT = 3000,
+  MONGO = 'mongodb://127.0.0.1:27017/bitfilmsdb',
+  JWT_SECRET = '64b47cd7d12efe505db55667',
+  NODE_ENV,
+} = process.env;
+
+const regexp = /(http:\/\/(?:www.|(?!www))[A-z0-9-]+\.[^\s]+)|(https:\/\/(?:www.|(?!www))[A-z0-9-]+\.[^\s]+)/;
+
+const LIMITER = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+module.exports = {
+  PORT,
+  JWT_SECRET,
+  MONGO,
+  NODE_ENV,
+  LIMITER,
+  regexp,
+};
