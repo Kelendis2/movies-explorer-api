cb7d9aae5296bd7e1bfad4f4bc2acca198d7b731
Karina <115954079+Kelendis2@users.noreply.github.com>
2023-08-12 20:28:05 -0400

    Revert "Revert "Merge pull request #1 from Kelendis2/level-1""
    This reverts commit 96b46f48140ffc7d40d4c5cd69ce02da07d12fad.

---
+++ controllers/movies.js
@@ -0,0 +1,60 @@
+const { ValidationError } = require('mongoose').Error;
+const Movie = require('../models/movie');
+// Импорт авторских ошибок
+const {
+  NOT_FOUND_ID_ERROR,
+  FORBIDDEN_ERROR,
+  BAD_REQUEST_ERROR,
+  STATUS_OK,
+} = require('../utils/constants');
+const BadRequest = require('../utils/errors/BadRequest');
+const NotFound = require('../utils/errors/NotFound');
+const Forbidden = require('../utils/errors/Forbidden');
+
+// Контроллер запроса фильмов
+const getMovies = (req, res, next) => {
+  const { _id } = req.user;
+  Movie.find({ owner: _id})
+    .then((movies) => {
+      res
+        .status(STATUS_OK)
+        .send(movies);
+    })
+    .catch(next);
+};
+// Контроллер создания фильма в избранном
+const createMovies = (req, res, next) => {
+  const { _id } = req.user;
+  Movie.create({ owner: _id, ...req.body })
+    .then((movie) => {
+      res.send({ data: movie });
+    })
+    .catch((err) => {
+      if (err instanceof ValidationError) {
+        next(new BadRequest(BAD_REQUEST_ERROR));
+      } else {
+        next(err);
+      }
+    });
+};
+// Контроллер удаление фильма из избранного
+const deleteMovie = (req, res, next) => {
+  const { movieId } = req.params;
+  const { _id } = req.user;
+  Movie.findById(movieId)
+    .orFail(new NotFound(NOT_FOUND_ID_ERROR))
+    .then((movie) => {
+      if (movie.owner.toString() !== _id) {
+        return Promise.reject(new Forbidden(FORBIDDEN_ERROR));
+      }
+      return Movie.deleteOne(movie)
+        .then(() => res.send({ message: 'Фильм успешно удален' }));
+    })
+    .catch(next);
+};
+
+module.exports = {
+  getMovies,
+  createMovies,
+  deleteMovie,
+};
---
+++ utils/constants.js
@@ -0,0 +1,33 @@
+const JWT_SECRET = '64b47cd7d12efe505db55667';
+
+const ERROR_CODE_UNIQUE = 11000;
+const STATUS_OK = 200;
+const STATUS_OK_201 = 201;
+
+const DUPLICATED_USER_ERROR = 'Пользователь с таким email уже зарегистрирован';
+const LOGIN_ERROR = 'Неправильная почта или пароль';
+const NOT_FOUND_USER_ERROR = 'Указанный пользователь не найден.';
+const BAD_REQUEST_USER_ERROR = 'Переданы некорректные данные при создании пользователя';
+const BAD_REQUEST_ERROR = 'Переданы некорректные данные';
+const NOT_FOUND_ID_ERROR = 'Ресурс с таким ID не найден.';
+const FORBIDDEN_ERROR = 'Недостаточно прав.';
+const SERVER_ERROR = 'На сервере произошла ошибка.';
+const VALIDATION_URL_ERROR = 'Некорректный URL';
+const VALIDATION_EMAIL_ERROR = 'Некорректный email';
+
+module.exports = {
+  JWT_SECRET,
+  NOT_FOUND_ID_ERROR,
+  DUPLICATED_USER_ERROR,
+  FORBIDDEN_ERROR,
+  SERVER_ERROR,
+  LOGIN_ERROR,
+  VALIDATION_URL_ERROR,
+  VALIDATION_EMAIL_ERROR,
+  NOT_FOUND_USER_ERROR,
+  BAD_REQUEST_USER_ERROR,
+  BAD_REQUEST_ERROR,
+  STATUS_OK,
+  ERROR_CODE_UNIQUE,
+  STATUS_OK_201,
+};
